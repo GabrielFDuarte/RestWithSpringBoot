c60ef2b94b6580126678a7b65728556c3568bb07
GabrielFDuarte <enggfduarte@gmail.com>
2022-06-23 11:02:58 -0300

    Added Migrations support

---
+++ 07 RestWithSpringBootUdemy Migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rio de Janeiro - RJ - Brasil', 'Gabriel', 'Male', 'Duarte'),
+	(2, 'Rio de Janeiro - RJ - Brasil', 'Aline', 'Female', 'Dias da Silva');
