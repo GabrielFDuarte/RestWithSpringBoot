30d6655d55103660fa3e46b79d35a1993f782ec6
GabrielFDuarte <enggfduarte@gmail.com>
2022-07-20 11:17:27 -0300

    Working with archetypes

---
+++ BONUS_RestWithSpringBootUdemy_GeneratedFromArchetype/rest-from-archetype/src/main/java/br/com/semeru/services/FileStorageService.java
@@ -0,0 +1,67 @@
+package br.com.semeru.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.semeru.config.FileStorageConfig;
+import br.com.semeru.exception.FileStorageException;
+import br.com.semeru.exception.MyFileNotFoundException;
+
+@Service
+public class FileStorageService {
+
+	private final Path fileStorageLocation;
+	
+	@Autowired
+	public FileStorageService(FileStorageConfig fileStorageConfig) {
+		
+		this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+		try {
+			Files.createDirectories(this.fileStorageLocation);
+		} catch (Exception e) {
+			throw new FileStorageException("Could not create the directory where the uploaded files would be stored.", e);
+		}
+	}
+
+	public String storeFile(MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		
+		try {
+			if (fileName.contains("..")) {
+				throw new FileStorageException("Sorry. Filename contains invalid path sequence: " + fileName);
+			}
+			
+			//Linhas que definem o destino do armazenamento
+			Path targetLocation = this.fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			
+			return fileName;
+		} catch (Exception e) {
+			throw new FileStorageException("Could not store file " + fileName + ". Please try again.", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			//Linhas que definem a origem da leitura do arquivo
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("File not found: " + fileName);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found: " + fileName, e);
+		}
+	}
+}
